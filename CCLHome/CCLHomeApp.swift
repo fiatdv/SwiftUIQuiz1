cc2bf618c7c87059435b0cdad84720154f4c9d1c
Felipe Gomez <fiatdv>
2022-08-04 13:55:15 -0400

    Initial Commit

---
+++ CCLHome/CCLHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCLHomeApp.swift
+//  CCLHome
+//
+//  Created by Felipe on 8/4/22.
+//
+
+import SwiftUI
+
+@main
+struct CCLHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
